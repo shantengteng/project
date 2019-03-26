134fb76aa7681235072ef1541a1e8991d1d0a459
shantengteng <shantengteng123>
2019-03-26 17:02:41 +0800

    springboot项目redis相关配置

---
+++ springboot-mybatis-redis/src/main/java/com/stt/springboot/utils/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.stt.springboot.utils;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+/**
+ *
+ * @author WangBin
+ * @version V1.0
+ * @date 2017.12.05
+ */
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+    /**
+     *  设置 redis 数据默认过期时间
+     *  设置@cacheable 序列化方式
+     * @return
+     */
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration(){
+        FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
+        configuration = configuration.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(fastJsonRedisSerializer)).entryTtl(Duration.ofDays(30));
+        return configuration;
+    }
+}
